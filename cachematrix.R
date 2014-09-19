8a57e03435a41705e0fb493a22b443b4b6bfb2b1
Arpan Soparkar <arpanscode@gmail.com>
2014-09-19 15:53:27 -0700

    programming assignment 2
    makeCacheMatrix() stores the original and inverted matrices, along with
    functions to retrieve those values.
    
    cacheSole() calls functions defined in the former function above. If
    inverse matrix is not set, cacheSolve() calculates it and stores it in
    cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMetrix() and cacheSolve() functions, which compute and cache inverse of the input matrix
 
-## Write a short comment describing this function
+## makeCacheMetrix() acts as the cache, that stores the original and inverted matrices. It also stores getinv and setinv()
+## functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        answer <- NULL
+        set <- function(y) {
+                x <<- y
+                answer <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) answer <<- inv
+        getinv <- function() answer
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSole() calls functions defined in teh former function above. If inverse matrix is not set, 
+## cacheSolve() calculates it and stores it in cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        answer <- x$getinv()
+        if(!is.null(answer)) {
+                message("getting cached data")
+                return(answer)
+        }
+        original <- x$get()
+        answer <- solve(original)
+        x$setinv(answer)
+        answer
 }
